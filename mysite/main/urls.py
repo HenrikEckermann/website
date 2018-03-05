bceca92d41262146892b58d6c8f8366ab25545eb
HenrikEckermann <henrikeckermann87@gmail.com>
2018-03-05 08:37:34 +0100

    update

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(),name='index'),
     url(r'^log_reg_r$', views.Lr_rView.as_view(),name='lr_r'),
     url(r'^linear_r_p$', views.Linear_rView.as_view(),name='linear_r'),
-    url(r'^xyplot$', views.XyplotView.as_view(),name='xyplot_r'),
+    url(r'^xyplot$', views.Xyplot_rView.as_view(),name='xyplot_r'),
     url(r'^about/$', views.AboutView.as_view(),name='about'),
 ]
