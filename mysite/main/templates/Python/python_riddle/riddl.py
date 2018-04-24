f6311d5e5afde9076187580efd1142d3c8c69b85
HenrikEckermann <henrikeckermann87@gmail.com>
2018-04-24 23:26:33 +0200

    update

--- mysite/main/templates/Python/python_riddle/riddl.py
+++ mysite/main/templates/Python/python_riddle/riddl.py
@@ -1,45 +1,52 @@
-#Go to http://www.pythonchallenge.com/ and get going
+# Go to http://www.pythonchallenge.com/ and get going
+
+# Preparation
+import os
+# set working directory
+directory = os.path.expanduser(
+    "~/Documents/workspace/website/mysite/main/templates/python/python_riddle")
+
 
 #######################################################
-#### Webscrapint maybe for later
-#store string in a file
+# Webscrapint maybe for later
+# store string in a file
 url = 'view-source:http://www.pythonchallenge.com/pc/def/ocr.html'
 #import bs4
 from bs4 import BeautifulSoup, Comment
 import requests
-#open the html file 
-with open('2/ocr.html') as html_file:
+# open the html file
+with open(f"{directory}/2/ocr.html") as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
-soup.findAll(text=lambda text:isinstance(text, Comment))
-match = soup.find_all(strin= lambda text:isinstance(text,Comment))
+soup.findAll(text=lambda text: isinstance(text, Comment))
+match = soup.find_all(strin=lambda text: isinstance(text, Comment))
 print(match)
 #####################################################
 
-import os
-os.getcwd()
-os.chdir('/Users/henrikeckermann/Documents/workspace/website/mysite/main/templates/python/python_riddle')
+### -0- ###
+2**38
+change_the_url_to = 'http://www.pythonchallenge.com/pc/def/274877906944.html'
 
 ### -1- ###
-#store string in variable
+# store string in variable
 s = 'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
 
-#create empty string to later store solution
+# create empty string to later store solution
 solution = ''
-#create string that contains all 26 characters
+# create string that contains all 26 characters
 import string
-base = string.ascii_lowercase*2
+base = string.ascii_lowercase * 2
 
-#recode using a loop
+# recode using a loop
 for i in range(len(s)):
     if s[i] not in base:
         solution += s[i]
     else:
-        solution += base[base.find(s[i])+2]
+        solution += base[base.find(s[i]) + 2]
 print(solution)
 
 ### -2- ###
-#store string in s
-with open('2/source.txt', 'r') as source:
+# store string in s
+with open(f"{directory}/2/source.txt", 'r') as source:
     s = source.read()
 
 d = {}
@@ -57,39 +64,35 @@ print(solution)
 
 ### -3- ###
 from urllib.request import urlopen
-#store hmtl code in a file
+# store hmtl code in a file
 url = 'http://www.pythonchallenge.com/pc/def/equality.html'
 f = urlopen(url).read()
 s = str(f)
-
-#extract comment 
+# extract comment
 import re
 pattern = re.compile(r'(<!--)(.*)(-->)')
 match = pattern.findall(str(f))
 print(match)
 temp = match[0][1]
 temp = temp.strip('\\n')
-s = temp.replace('\\n','')
-
-
-
+s = temp.replace('\\n', '')
 solution = ''
-for i in range(3,len(s)-3):
-    if s[i].islower() & s[i-3:i].isupper() & s[i+1:i+4].isupper():
-        if sum([1 for c in s[i-4:i+5] if c.isupper()])==6:
+for i in range(3, len(s) - 3):
+    if s[i].islower() & s[i - 3:i].isupper() & s[i + 1:i + 4].isupper():
+        if sum([1 for c in s[i - 4:i + 5] if c.isupper()]) == 6:
             solution += s[i]
 print(solution)
 
 
 ### -4- ###
 from urllib.request import urlopen
-#store hmtl code in a file
+# store hmtl code in a file
 url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
 nothing = '63579'
 counter = 0
-#the solution was printen unter nothing = 66831 and is: 'peak.html'
+# the solution was printen unter nothing = 66831 and is: 'peak.html'
 while counter < 100:
-    f = urlopen(url+nothing).read()
+    f = urlopen(url + nothing).read()
     s = str(f)
     print(s)
     #solution += s[0]
@@ -98,25 +101,20 @@ while counter < 100:
 
 
 ### -5- ###
-#pickle...
-import os
+# pickle...
 import pickle
-
-#os.chdir('python_riddle')
-pickle_in = open('5/banner.p', 'rb')
+pickle_in = open(f"{directory}/5/banner.p", 'rb')
 code = pickle.load(pickle_in)
 pickle_in.close()
 for l in code:
     s = ''
     for t in l:
-        s += t[1]*t[0]
+        s += t[1] * t[0]
     print(s)
 
-    
+
 ### -6- ###
-import os
-os.getcwd()
-os.chdir('/Users/henrikeckermann/Documents/workspace/website/mysite/main/templates/Python/python_riddle/6/')
+os.chdir(f"{directory}/6/")
 os.listdir()
 import zipfile
 
@@ -125,29 +123,28 @@ f_name = '90052.txt'
 solution = []
 with zipfile.ZipFile('channel.zip', 'r') as myzip:
     r = len(myzip.namelist())
-    while counter < r:
+    while f_name in myzip.namelist():
         with myzip.open(f_name) as f:
             s = f.read().decode()
             comment = myzip.getinfo(f_name).comment.decode()
             solution.append(comment)
             f_name = s.split()[-1] + '.txt'
-            counter += 1
-            
-solution
-s = ''
+
+
+solution_final = []
 for i in solution:
-    s += i
-print(s)
-solution = 'oxygen'
+    if i.isalpha() and i not in solution_final:
+        solution_final.append(i)
+solution_final
 
 ### -7- ###
 from PIL import Image
-im = Image.open('7/oxygen.png')
+im = Image.open(f"{directory}/7/oxygen.png")
 im.size
 im
 print(im.format, im.size, im.mode)
 
-bb = im.crop((0,44, 629, 51))
+bb = im.crop((0, 44, 629, 51))
 bb.save('bb.png')
 bb
 bb2 = bb.copy()
@@ -156,7 +153,7 @@ code = []
 width, length = bb.size
 for w in range(width):
     for l in range(length):
-        code.append(bb.getpixel((w,l)))
+        code.append(bb.getpixel((w, l)))
 
 
 solution = ''
@@ -171,55 +168,115 @@ nl = [105, 110, 116, 101, 103, 114, 105, 116, 121]
 print([chr(x) for x in nl])
 
 ### -8- ###
-#I use bs4 to get some practice in using this useful tool (it will be more pythonic to use urlib)
-from bs4 import Comment, BeautifulSoup as bs 
+# I use bs4 to get some practice in using this useful tool (it will be more pythonic to use urlib)
+from bs4 import Comment, BeautifulSoup as bs
 import requests
-#specify url level 8
+import ast
+# specify url level 8
 url = 'http://www.pythonchallenge.com/pc/def/integrity.html'
-#load the html file and store in r
+# load the html file and store in r
 r = requests.get(url)
 soup = bs(r.text, 'lxml')
-#store comment in result
-result = soup.findAll(text=lambda text:isinstance(text, Comment))
+# store comment in result
+result = soup.findAll(text=lambda text: isinstance(text, Comment))
 s = result[0][6:]
-#extract un and pw
+# extract un and pw
 import re
 pattern = re.compile(r'(.*)(\spw:)(.*)')
 un, nothing, pw = pattern.findall(s)[0]
-#decompress bytes (here I had some trouble finding out about bz2 and how to decompress the file that I originally had (because of '\\'))
+# decompress bytes (here I had some trouble finding out about bz2 and how to decompress the file that I originally had (because of '\\'))
 import bz2
 nun = "'{}".format(un)
 npw = pw[1:]
-import ast
 username = ast.literal_eval('b%s' % nun)
 password = ast.literal_eval('b%s' % npw)
-print(bz2.decompress(username),bz2.decompress(password))
+print(bz2.decompress(username), bz2.decompress(password))
 
 
 ### -9- ###
-import requests, re
+import re
 from bs4 import Comment, BeautifulSoup as bs
-from requests.auth import HTTPBasicAuth
-from PIL import Image
-import os
+from PIL import Image, ImageDraw
 
 
-with open('9/source.html') as html_file:
+with open(f"{directory}/9/source.html") as html_file:
     soup = bs(html_file, 'lxml')
-#grab comment 
-c = soup.findAll(text=lambda text:isinstance(text, Comment))[0][24:]
+# grab comment
+c = soup.findAll(text=lambda text: isinstance(text, Comment))[0][24:]
 c = c.replace('\n', '')
 pattern = re.compile(r'(.*)(second:)(.*)')
 sfirst, nothing, ssecond = pattern.findall(c)[0]
-len(sfirst.split(','))
-first = [(int(sfirst.split(',')[i]), int(sfirst.split(',')[i+1])) for i in range(0, 442, 2)]
-second = [int(x) for x in ssecond.split(',')]
-len(second) 
-len(first)
-im = Image.open('9/good.jpg')
+first = [int(i) for i in sfirst.split(",")]
+second = [int(i) for i in ssecond.split(",")]
+first_coord = [(first[x], first[x + 1]) for x in range(0, 442, 2)]
+second_coord = [(second[x], second[x + 1]) for x in range(0, 112, 2)]
+final_coord = first_coord + second_coord
+im = Image.open(f"{directory}/9/good.jpg")
 im_c = im.copy()
-width, length = im.size
-print(width, length)
-for i in first:
-    im.putpixel(i, (0, 0, 0, 255))
+draw = ImageDraw.Draw(im)
+for i in range(276):
+    draw.line(final_coord[i] + final_coord[i + 1], fill=(255, 0, 0), width=4)
+
 im
+# The solution is bull.
+
+
+### -10- ###
+
+# If you click on the picture you get a list: a = [1, 11, 21, 1211, 111221...
+# Find a[30]: The algorithm to create the next element is: For each digit in 
+# previous list element count how often it appears in a row: 1 --> 1 x 1 --> 
+# 11. Or 21 --> 1 x 2 + 1 x 1 -> 1211 etc...
+
+a = ["1"]
+while len(a) != 31:
+    new = ""
+    skip = 1
+    # for the last element of a, iterate over the digits:
+    for n, i in enumerate(a[-1]):
+        # skip: to be able to skip digit if it was already counted
+        if skip != 1:
+            skip -= 1
+            continue
+        # counter: to count how often a digit comes in a row
+        counter = 1
+        # ind: to compare with next digit
+        ind = n + 1
+        # count how often i comes in a row and add that to "new":
+        while ind <= len(a[-1]):
+            # if we reach the last element of the string a[-1], 
+            # we need to add what was counted up before
+            if ind == len(a[-1]):
+                new += f"{str(counter)}{i}"
+                skip = counter
+                break
+            if a[-1][ind] == i:
+                counter += 1
+                ind += 1
+            else:
+                new += f"{str(counter)}{i}"
+                skip = counter
+                break
+    a.append(new)
+
+
+# the solution is:
+len(a[30])
+
+### -11- ###
+# extract html code
+url = "http://www.pythonchallenge.com/pc/return/5808.html"
+soup = bs(requests.get(url, auth=('huge', 'file')).text, 'lxml')
+print(soup.prettify())
+# extract image link to open it 
+img_link = soup.find("img")
+img_url = url.replace("5808.html", img_link["src"])
+img = Image.open(requests.get(img_url, stream = True, auth=('huge', 'file')).raw)
+img
+width, height = img.size
+pixel = []
+for w in range(width):
+    for h in range(height):
+        pixel.append(img.getpixel((w, h)))
+
+pixel 
--- mysite/main/templates/r/reporting.R
+++ mysite/main/templates/r/reporting.R
@@ -1,32 +1,71 @@
+library(pander)
+library(glue)
 
-#report estimates + anova table stats
-print_estimates <- function(model, name, complete=TRUE, method='PB') {
+
+
+### --- for LME --- ###
+
+# returns apa p value as a string 
+report_p <- function(p) {
+  ifelse(p < 0.001, paste('_p_ <', '.001'),  paste('_p_ =', substring(as.character(p), 2,5)))
+}
+
+# same as above but returns *, ** or ***
+report_star <- function(p) {
+  ifelse(p < 0.001, return('***'), ifelse(p < 0.01, return('**'), ifelse(p < 0.05, return('*'), ' ')))
+}
+
+
+
+#I use this function not for the analysis but only for reporting the estimates or test statistics.
+print_estimates <- function(model, name, complete=TRUE, method='PB', fes='none', conf=95) {
   
+  #determine the ci print 
+  ci <- ifelse(conf==95, '95% CI', ifelse(conf==99, '99% CI', '99.9% CI'))
+  #assign ci if delivered
+  if(fes=='none') {
+    lower <- ''
+    upper <- ''
+  } else {
+    lower <- round(fes[fes$coefficient==name,'lower'], digits=2)
+    upper <- round(fes[fes$coefficient==name,'upper'], digits=2)
+  }
+
+  #assign method printversion 
   df_sig <- ifelse(method=='PB', '_PBtest_', ifelse(method=='KR', '_F_', '$\\chi^2$'))
   
-  ifelse(method=='PB', ifelse(!complete, glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))})'), glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))}), {df_sig} = {format(model$anova_table[name,1], digits=ifelse(model$anova_table[name,1]<1, 2, ifelse(model$anova_table[name,1]<10, 3, ifelse(model$anova_table[name,1]<100, 4, 5))))}, _p_ {report_p(model$anova_table[name,4])})')), ifelse(method=='KR', ifelse(!complete, glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))})'), glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))}), {df_sig}({model$anova_table[name,1]},{format(model$anova_table[name,2], digits=ifelse(model$anova_table[name,2]<1, 2, ifelse(model$anova_table[name,2]<10, 3, ifelse(model$anova_table[name,2]<100, 4, 5))))}) = {format(model$anova_table[name,3], digits=ifelse(model$anova_table[name,3]<1, 2, ifelse(model$anova_table[name,3]<10, 3, ifelse(model$anova_table[name,3]<100, 4, 5))))}, _p_ {report_p(model$anova_table[name,4])})')), ifelse(!complete, glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))})'), glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))}), {df_sig}({format(model$anova_table[name,3], digits=ifelse(model$anova_table[name,3]<1, 2, ifelse(model$anova_table[name,3]<10, 3, ifelse(model$anova_table[name,3]<100, 4, 5))))}) = {format(model$anova_table[name,2], digits=ifelse(model$anova_table[name,2]<1, 2, ifelse(model$anova_table[name,2]<10, 3, ifelse(model$anova_table[name,2]<100, 4, 5))))}, _p_ {report_p(model$anova_table[name,4])})'))))
+  # which string to return. 
+  ifelse(method=='PB', ifelse(!complete, glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))})'), glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))}), {df_sig} = {format(model$anova_table[name,1], digits=ifelse(model$anova_table[name,1]<1, 2, ifelse(model$anova_table[name,1]<10, 3, ifelse(model$anova_table[name,1]<100, 4, 5))))},  {report_p(model$anova_table[name,4])})')), ifelse(method=='KR', ifelse(!complete, glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))})'), glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))}), {df_sig}({model$anova_table[name,1]},{format(model$anova_table[name,2], digits=ifelse(model$anova_table[name,2]<1, 2, ifelse(model$anova_table[name,2]<10, 3, ifelse(model$anova_table[name,2]<100, 4, 5))))}) = {format(model$anova_table[name,3], digits=ifelse(model$anova_table[name,3]<1, 2, ifelse(model$anova_table[name,3]<10, 3, ifelse(model$anova_table[name,3]<100, 4, 5))))}, {report_p(model$anova_table[name,4])}, {ci} [{lower}, {upper}])')), ifelse(!complete, glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))})'), glue::glue('Estimate = {format(summary(model)$coefficients[name,1], digits=ifelse(summary(model)$coefficients[name,1]<1, 2, ifelse(summary(model)$coefficients[name,1]<10, 3, ifelse(summary(model)$coefficients[name,1]<100, 4, 5))))}({format(summary(model)$coefficients[name,2], digits=ifelse(summary(model)$coefficients[name,2]<1, 2, ifelse(summary(model)$coefficients[name,2]<10, 3, ifelse(summary(model)$coefficients[name,2]<100, 4, 5))))}), {df_sig}({format(model$anova_table[name,3], digits=ifelse(model$anova_table[name,3]<1, 2, ifelse(model$anova_table[name,3]<10, 3, ifelse(model$anova_table[name,3]<100, 4, 5))))}) = {format(model$anova_table[name,2], digits=ifelse(model$anova_table[name,2]<1, 2, ifelse(model$anova_table[name,2]<10, 3, ifelse(model$anova_table[name,2]<100, 4, 5))))}, {report_p(model$anova_table[name,4])})'))))
   
 }
 
-#report only model comparison stats from the anova table
+#report only model comparison stats from the anova table (required for factors with more than 2 levels)
 print_comp <- function(model, name, method='PB') {
   
   df_sig <- ifelse(method=='PB', '_PBtest_', ifelse(method=='KR', '_F_', '$\\chi^2$'))
   
-  ifelse(method=='PB', glue::glue('{df_sig} = {format(model$anova_table[name,1], digits=ifelse(model$anova_table[name,1]<1, 2, ifelse(model$anova_table[name,1]<10, 3, ifelse(model$anova_table[name,1]<100, 4, 5))))}, _p_ {report_p(model$anova_table[name,4])})'), ifelse(method=='KR',  glue::glue('{df_sig}({model$anova_table[name,1]},{format(model$anova_table[name,2], digits=ifelse(model$anova_table[name,2]<1, 2, ifelse(model$anova_table[name,2]<10, 3, ifelse(model$anova_table[name,2]<100, 4, 5))))}) = {format(model$anova_table[name,3], digits=ifelse(model$anova_table[name,3]<1, 2, ifelse(model$anova_table[name,3]<10, 3, ifelse(model$anova_table[name,3]<100, 4, 5))))}, _p_ {report_p(model$anova_table[name,4])})'), glue::glue('{df_sig}({format(model$anova_table[name,3], digits=ifelse(model$anova_table[name,3]<1, 2, ifelse(model$anova_table[name,3]<10, 3, ifelse(model$anova_table[name,3]<100, 4, 5))))}) = {format(model$anova_table[name,2], digits=ifelse(model$anova_table[name,2]<1, 2, ifelse(model$anova_table[name,2]<10, 3, ifelse(model$anova_table[name,2]<100, 4, 5))))}, _p_ {report_p(model$anova_table[name,4])})')))
+  ifelse(method=='PB', glue::glue('{df_sig} = {format(model$anova_table[name,1], digits=ifelse(model$anova_table[name,1]<1, 2, ifelse(model$anova_table[name,1]<10, 3, ifelse(model$anova_table[name,1]<100, 4, 5))))},  {report_p(model$anova_table[name,4])})'), ifelse(method=='KR',  glue::glue('{df_sig}({model$anova_table[name,1]},{format(model$anova_table[name,2], digits=ifelse(model$anova_table[name,2]<1, 2, ifelse(model$anova_table[name,2]<10, 3, ifelse(model$anova_table[name,2]<100, 4, 5))))}) = {format(model$anova_table[name,3], digits=ifelse(model$anova_table[name,3]<1, 2, ifelse(model$anova_table[name,3]<10, 3, ifelse(model$anova_table[name,3]<100, 4, 5))))}, {report_p(model$anova_table[name,4])})'), glue::glue('{df_sig}({format(model$anova_table[name,3], digits=ifelse(model$anova_table[name,3]<1, 2, ifelse(model$anova_table[name,3]<10, 3, ifelse(model$anova_table[name,3]<100, 4, 5))))}) = {format(model$anova_table[name,2], digits=ifelse(model$anova_table[name,2]<1, 2, ifelse(model$anova_table[name,2]<10, 3, ifelse(model$anova_table[name,2]<100, 4, 5))))}, {report_p(model$anova_table[name,4])})')))
   
 }
 
 
 
+### --- for PCA --- ###
 
-#function for returning p value 
-report_p <- function(p) {
-  ifelse(p < 0.001, paste('<', '.001'),  paste('=', substring(as.character(p), 2,5)))
+# make sig loadings bold
+pca_bolding <- function(x, cut = 0.4) {
+  ifelse(x>=cut, glue("**{round(x, digits = 2)}**"), glue("{round(x, digits = 2)}"))
 }
 
-
-
+# return rmarkdown pca table 
+pca_table <- function(pca_object, cut = 0.4, caption = 'Component loadings') {
+  pca_tab <- as.data.frame(unclass(pca_object$loadings))
+  fac_names <- rownames(pca_tab)
+  # format loadings
+  pca_tab <- mutate_all(pca_tab, funs(pca_bolding))
+  rownames(pca_tab) <- fac_names
+  pander(pca_tab, caption = caption)
+}
 
 
 
